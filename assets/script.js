34327bc1a100cca1d8d211b31d3eabc4f64302da
Brad <Bradrockk@gmail.com>
2023-11-22 02:24:46 -0500

    Fixed cards for weather layout, began working towards writing data to cards

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,35 @@
 // global variables
 let lat = ""
 let lon = ""
-const city = "new york"
+
 
 
 // code ran when page has loaded.
 function onload(){
-    const submitbtn = document.getElementById("submit")
-    submitbtn.addEventListener("click",GetCordinates)
-    
+    const submitbtn = document.getElementById("submit");
+    submitbtn.addEventListener("click",GetCordinates);
 }
 
 
 
 
-// grab the cordinates of the city based on the entered name.
+// grab the cordinates of the city based on the entered name, sends them to weather call.
 async function GetCordinates(){
-    const cordsCall = await fetch ("http://api.openweathermap.org/geo/1.0/direct?q=" + (city) + "&limit=1&appid=513b2f121ba24b289f75ea98532934e4")
-    let cords = await cordsCall.json()
-    lat = cords[0].lat
-    lon = cords[0].lon
-    GetWeather()
+    const city = document.getElementById("cityentry").value;
+    const cordsCall = await fetch ("http://api.openweathermap.org/geo/1.0/direct?q=" + (city) + "&limit=1&appid=513b2f121ba24b289f75ea98532934e4");
+    let cords = await cordsCall.json();
+    lat = cords[0].lat;
+    lon = cords[0].lon;
+    GetWeather();
 }
 
-// Grab the weather of the city based on the cordinates from GetCordinates()
+// Grab the weather of the city based on the cordinates from GetCordinates(), and sends them to the proper div card OR the current day.
  async function GetWeather(){
  const ForecastCall = await fetch ("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=513b2f121ba24b289f75ea98532934e4");
- let Forecast = ForecastCall.json()
+ let Forecast = ForecastCall.json();
+ const firstday = document.getElementById("box1")
+ console.log(firstday)
 
- console.log(Forecast)
+ console.log(Forecast);
  }
 
